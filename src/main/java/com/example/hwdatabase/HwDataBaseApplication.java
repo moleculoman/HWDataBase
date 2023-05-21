aad111b2ac24d9ca92d3fd8a78425789b2bdca68
moleculoman <Xz2@ZBS@mhSFpg7>
2023-05-21 20:29:30 +0700

    initial

---
+++ src/main/java/com/example/hwdatabase/HwDataBaseApplication.java
@@ -0,0 +1,12 @@
+package com.example.hwdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwDataBaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HwDataBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hwdatabase/HwDataBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwDataBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
